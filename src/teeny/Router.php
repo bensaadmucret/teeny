400032e4e110bc474cc3673d166a21434042599a
Mohammed Bensaad <bensaadmucret@gmail.com>
2020-06-11 13:19:53 +0200

    mise a jour route.php

---
+++ src/teeny/Router.php
@@ -0,0 +1,64 @@
+<?php
+namespace teeny;
+
+use Mezzio\Router\FastRouteRouter;
+use Psr\Http\Message\ServerRequestInterface;
+use teeny\Router\Route;
+use Mezzio\Router\Route as Mezzio;
+use teeny\Router\MiddlewareApp;
+
+/**
+ * Class Router
+ * @package teeny
+ */
+/**
+ * Class Router
+ * @package Framework
+ * Register and match Route
+ */
+class Router
+{
+    private $router;
+
+    public function __construct()
+    {
+        $this->router = new FastRouteRouter();
+    }
+
+    /**
+     * @param string $path
+     * @param string|callable $callable
+     * @param string $name
+     */
+    public function get(string $path, $callable, string $name)
+    {
+        $this->router->addRoute(new Mezzio($path, new MiddlewareApp($callable), ['GET'], $name));
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @return Route|null
+     */
+    public function match(ServerRequestInterface $request): ?Route
+    {
+        $result = $this->router->match($request);
+        if ($result->isSuccess()) {
+            return new Route(
+                $result->getMatchedRouteName(),
+                $result->getMatchedRoute()->getMiddleware()->getCallback(),
+                $result->getMatchedParams()
+            );
+        }
+        return null;
+    }
+
+    /**
+     * @param string $name
+     * @param array $params
+     * @return null|string
+     */
+    public function generateUri(string $name, array $params): ?string
+    {
+        return $this->router->generateUri($name, $params);
+    }
+}
---
+++ src/teeny/Router/Route.php
@@ -0,0 +1,61 @@
+<?php
+namespace teeny\Router;
+
+/**
+ * Class Route
+ * @package teeny\Router
+ * Represent a route
+ */
+class Route
+{
+    /**
+     * @var string
+     */
+    private $name;
+    /**
+     * @var callable
+     */
+    private $callable;
+    /**
+     * @var string[]
+     */
+    private $params;
+
+    /**
+     * Route constructor.
+     * @param string $name
+     * @param string|callable $callable
+     * @param array $params
+     */
+    public function __construct(string $name, $callable, array $params = [])
+    {
+        $this->name = $name;
+        $this->callable = $callable;
+        $this->params = $params;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string|callable
+     */
+    public function getCallback()
+    {
+        return $this->callable;
+    }
+
+    /**
+     * Get URL parameters
+     * @return string[]
+     */
+    public function getParams(): array
+    {
+        return $this->params;
+    }
+}
